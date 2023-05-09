f40eb71f5e7771d3ac72868107f6810750c8fa68
CHINMAY <CHINMAY@LAPTOP-PRAH6917>
2023-05-10 00:03:41 +0530

    New Demo Project

---
+++ GithubDemo/src/com/automation/gitdemo/Demo.java
@@ -0,0 +1,17 @@
+package com.automation.gitdemo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a = 45;
+		int b = 88;
+		
+		int c = a+b;
+		System.out.println(c);
+
+	}
+
+}
