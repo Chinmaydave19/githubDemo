5a3afa9d74cc36783645f18a03a81da294ef9125
CHINMAY <CHINMAY@LAPTOP-PRAH6917>
2023-05-10 22:25:40 +0530

    Array List

--- GithubDemo/src/com/automation/gitdemo/Demo.java
+++ GithubDemo/src/com/automation/gitdemo/Demo.java
@@ -1,26 +1,38 @@
 package com.automation.gitdemo;
 
+import java.util.ArrayList;
+
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+
+
 		int a = 45;
 		int b = 88;
-		
-		
+
+
 		int c = a+b;
 		System.out.println(c);
-		
-//		Jayastu Patil
-		
+
+		//Jayastu Patil
+
 		String name = "Jayastu";
 		String surname = "Patil";
-		
+
 		String fullname = name+surname;
-		
+
 		System.out.println(fullname);
+
+
+		//New code on 10/05/23
+		
+		ArrayList<String> obj = new ArrayList<String>();
+		obj.add("Vinoth");
+		obj.add("Kiya");
+		obj.add("Veer");
+		
+		System.out.println(obj);
 
 	}
 
